5433a34be347bebde8167ff76737dae3ccca8a7d
Topi Hellberg <thh.projects@proton.me>
2023-11-29 11:39:21 +0200

    Modify filename for tests that use unittest, add a second testfile that uses pytest

--- calculations.py
+++ calculations.py
@@ -1,8 +1,10 @@
 def add(x, y):
     return x + y
 
+
 def multiply(x, y):
     return x * y
+
 
 def power(x, y):
     """Calculate x raised to the power y without using math.pow."""
---
+++ test_calculations.py
@@ -0,0 +1,14 @@
+import pytest
+from calculations import *
+
+
+class TestCalculations:
+    def test_add(self):
+        assert add(2, 3) == 5
+        assert add(2, 2) != 5
+
+    def test_multiply(self):
+        assert multiply(4, 5) == 20
+
+    def test_power(self):
+        assert power(3, 5) == 243
